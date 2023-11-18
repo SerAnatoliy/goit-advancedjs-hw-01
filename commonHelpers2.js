aa7b603816d37222b452193cde192e02de739e53
SerAnatoliy <132679856+SerAnatoliy@users.noreply.github.com>
2023-11-18 18:13:10 +0000

    Deploying to gh-pages from @ SerAnatoliy/goit-advancedjs-hw-01@5255c7ec819cb997e0879cc4cb1ffcf67571f2c0 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const m=document.querySelector("iframe"),e=new r(m),t="videoplayer-current-time",n=function({seconds:o}){localStorage.setItem(t,o)};e.on("timeupdate",a(n,1e3));const c=+localStorage.getItem(t);e.setCurrentTime(c);
+//# sourceMappingURL=commonHelpers2.js.map
