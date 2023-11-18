5255c7ec819cb997e0879cc4cb1ffcf67571f2c0
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-11-18 19:12:44 +0100

    second_task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe)
+const stopTime = 'videoplayer-current-time'
+
+const Play = function ({ seconds }) {
+    localStorage.setItem(stopTime, seconds)
+}
+
+player.on('timeupdate', throttle(Play, 1000))
+
+const currentTime = +localStorage.getItem(stopTime)
+player.setCurrentTime(currentTime)
