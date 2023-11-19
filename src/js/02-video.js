d7c1578023bc14c07f758761ea92d81f0492b389
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-11-19 17:50:42 +0100

    third_task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle'
 
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
-const stopTime = 'videoplayer-current-time'
+const LS_KEY = 'videoplayer-current-time'
 
 const Play = function ({ seconds }) {
-    localStorage.setItem(stopTime, seconds)
+    localStorage.setItem(LS_KEY, seconds)
 }
 
 player.on('timeupdate', throttle(Play, 1000))
 
-const currentTime = +localStorage.getItem(stopTime)
+const currentTime = +localStorage.getItem(LS_KEY)
 player.setCurrentTime(currentTime)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,48 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
+
+const rec = {
+    form: document.querySelector('.js-feedback-form'),
+    input: document.querySelector('.js-feedback-form input'),
+    message: document.querySelector('.js-feedback-form textarea')
+};
+
+let formData = {}
+const STOR_KEY = 'feedback-form-state'
+
+getSaveData()
+
+rec.form.addEventListener('input', throttle(handlerInput, 500))
+rec.form.addEventListener('submit', handlerSubmit)
+
+function handlerInput() {
+    formData = {
+        email: rec.input.value,
+        message: rec.message.value,
+
+    }
+    localStorage.setItem(STOR_KEY, JSON.stringify(formData))
+}
+
+function handlerSubmit(def) {
+    def.preventDefault();
+
+    console.log(formData)
+
+    if (rec.input.value.length !== 0 && rec.message.value.length !== 0) {
+        def.currentTarget.reset()
+        localStorage.removeItem(STOR_KEY)
+    }
+    else {
+        alert('All fields must be filled')
+    }
+}
+
+function getSaveData() {
+    const lsData = JSON.parse(localStorage.getItem(STOR_KEY))
+    if (!lsData) {
+        return
+    }
+    rec.input.value = lsData.email ?? ''
+    rec.message.value = lsData.message ?? ''
+}
 
